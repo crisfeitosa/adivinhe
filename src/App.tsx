933feb33ed7da59669c26fa5f7352c16aa99dfcd
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-14 09:44:53 -0300

    Criando o Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './global.css'
+import { Header } from './components/Header'
 
-import styles from './app.css'
+import styles from './app.module.css'
 
 function App() {
   return (
-    <div>
-      <h1>Hello word</h1>
-      <button>Increment</button>
+    <div className={styles.container}>
+      <main>
+        <Header />
+      </main>
     </div>
   )
 }
