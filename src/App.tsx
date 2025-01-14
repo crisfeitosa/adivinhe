ab58f4e8b13ccb276803e52e75c81d774418f253
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-14 10:05:21 -0300

    Header dinâmico

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { Header } from './components/Header'
 import styles from './app.module.css'
 
 function App() {
+
+  function handleRestartGame() {
+    alert('Game restart')
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={handleRestartGame} />
       </main>
     </div>
   )
