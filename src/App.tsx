2f9101473f559587353e37f34954bd62123d9215
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-14 09:22:02 -0300

    Estilo Global

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './global.css'
+
+import styles from './app.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello word</h1>
+      <button>Increment</button>
+    </div>
+  )
+}
+
+export default App
